083df36ebb456710265da2d208f6fbd79928218f
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-25 20:05:40 +0530

    setUp Client

--- client/src/main/java/lk/ijse/dep11/ClientViewController.java
+++ client/src/main/java/lk/ijse/dep11/ClientViewController.java
@@ -1,6 +1,27 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.dep11.tm.Customer;
+
 public class ClientViewController {
 
 
+    public AnchorPane root;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtContact;
+    public Button btnSubmit;
+    public TableView <Customer>tblClient;
+    public Spinner <Integer> spn1;
+    public Spinner <Integer>spn2;
+    public Spinner <Integer>spn3;
+    public Spinner <Integer>spn4;
+
+    public void btnSubmitOnAction(ActionEvent actionEvent) {
+    }
 }
