ffffd5cd7226bcf68d58db534395eb0f0106bd69
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-25 19:24:10 +0530

    create class

--- shared/src/main/java/lk/ijse/dep11/Main.java
+++ shared/src/main/java/lk/ijse/dep11/Main.java
@@ -1,7 +1,15 @@
 package lk.ijse.dep11;
 
+import lk.ijse.dep11.tm.Customer;
+
+import java.util.ArrayList;
+
 public class Main {
+    ArrayList<Customer> customerlist = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    }
+    public void addCustomers(Customer customer){
     }
 }
