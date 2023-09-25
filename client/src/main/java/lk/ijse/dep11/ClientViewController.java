65d06be0c4fad2e3c6bf2d3981991945caeb2c0a
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-25 20:30:10 +0530

    setUp Client1

--- client/src/main/java/lk/ijse/dep11/ClientViewController.java
+++ client/src/main/java/lk/ijse/dep11/ClientViewController.java
@@ -5,8 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.dep11.tm.Customer;
+
+import java.util.ArrayList;
 
 public class ClientViewController {
 
@@ -21,7 +24,33 @@ public class ClientViewController {
     public Spinner <Integer>spn2;
     public Spinner <Integer>spn3;
     public Spinner <Integer>spn4;
+    public Button btnNew;
+
+
+
+    public void initialize(){
+        tblClient.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblClient.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblClient.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        txtId.setText(setupId());
+    }
 
     public void btnSubmitOnAction(ActionEvent actionEvent) {
+    }
+    public ArrayList<Customer> getCustomer(){
+        return Data.arr;
+    }
+    public String setupId(){
+        if(getCustomer().isEmpty()) return "E-001";
+        String lastId = getCustomer().get(getCustomer().size()-1).getId();
+        int id = Integer.parseInt(lastId.substring(2))+1;
+        return String.format("E-%03d",id);
+    }
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+        txtId.setText(setupId());
+        txtName.setText("");
+        txtContact.setText("");
+        txtName.requestFocus();
     }
 }
