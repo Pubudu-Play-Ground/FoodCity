dad5a588e2c8bc3454656845547dfd79fca5e647
Pubudu Ariyawansha <pubuduariyawansa@gmail.com>
2023-09-26 10:02:36 +0530

    update project

--- shared/src/main/java/lk/ijse/dep11/tm/Customer.java
+++ shared/src/main/java/lk/ijse/dep11/tm/Customer.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 
-public class Customer {
+public class Customer implements Serializable {
     String id;
     String name;
     String contact;
